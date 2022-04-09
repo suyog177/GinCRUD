3a63256d03e680249011087d2939f306138384c4
suyog177 <53820557+suyog177@users.noreply.github.com>
2022-04-10 03:20:09 +0530

    passing name in url and printing with hello
    handler to take a url parameter name and return hello world, {{name}}

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/", GetString)
+	r.GET("/:name", GetName)
 
 	r.Run()
 }
@@ -16,4 +17,10 @@ func main() {
 func GetString(c *gin.Context) {
 
 	c.String(200, "Hello world")
+}
+
+func GetName(c *gin.Context) {
+	name := c.Param("name")
+	msg := fmt.Sprintf("Hello world, %s", name)
+	c.String(200, msg)
 }
